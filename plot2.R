6271d09badf54c94372b18ef332e6e258314fca0
tbaker-sc <tbaker@sharecare.com>
2015-03-07 10:48:33 -0500

    Adding R script that produces plot 2 and the output from that script

---
+++ plot2.R
@@ -0,0 +1,36 @@
+plot2 <- function () {
+	
+	#First block of code confirms the file is present
+	#If not, it pulls down the file from the URL & unzips
+	#Function return immediately if file still isn't present after trying to load it from URL
+
+	if (!file.exists("household_power_consumption.txt")) {
+		fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+		download.file(fileUrl, destfile = "exdata-data-household_power_consumption.zip", method = "curl")
+		unzip("exdata-data-household_power_consumption.zip")
+		if (!file.exists("household_power_consumption.txt")) {
+			print("ERROR: Cannot open file")
+			return()
+		} 
+	}
+	#Read in data file
+	#Include values for seperator (;) and  na (?)   from original dataset
+	#Have it not convert the dates & times to factor. (as.is = TRUE)
+	full_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?", as.is=TRUE)
+	
+	#Subset the full_data data frame so we're only dealng with the 2 dates needed for this project
+	sub_data <- full_data[(full_data$Date == "2/2/2007" | full_data$Date == "1/2/2007"),]
+	
+	#Open the png graphics device that we'll be writing to
+	png("plot2.png")
+	
+	#Create the plot using the following options
+	#type = "l"  -- Change the type of graph from the default (scatterplot) to a line graph
+	#xlab = "<>"  -- changes the values of the label on the x-axis from the default	
+	#ylab = "<>"  -- changes the values of the label on the y-axis from the default
+	plot(strptime(paste(sub$Date, sub$Time), format = "%d/%m/%Y %H:%M:%S"), sub$Global_active_power, 
+		type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+	
+	#Close graphics device
+	dev.off()
+}
